5bd3aff0cefcc684a887b524b07da7a2606fb496
chenfeihao <chenfeihao@corp.netease.com>
2019-05-09 15:45:34 +0800

    @dev 初始化工程

---
+++ src/main/java/com/tinysakura/smartsearchbox/SmartSearchBoxApplication.java
@@ -0,0 +1,13 @@
+package com.tinysakura.smartsearchbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSearchBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSearchBoxApplication.class, args);
+	}
+
+}
